0a677cfd8b7d36ce93c5a47f30406e277ca1f9e9
Antonio Fulgêncio <antoniofulg@gmail.com>
2020-01-21 16:18:17 -0300

    Add author info and question creation date in question item

--- app/User.php
+++ app/User.php
@@ -40,4 +40,9 @@ class User extends Authenticatable
     public function questions() {
         return $this->hasMany(Question::class);
     }
+
+    public function getUrlAttribute() {
+        // return route("questions.show", $this->id);
+        return '#';
+    }
 }
