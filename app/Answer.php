58b43807f9e4f8dac57f85999018e76c8be0a18e
Antonio Fulgêncio <antoniofulg@gmail.com>
2020-01-22 18:03:44 -0300

    Add answers on show question page

--- app/Answer.php
+++ app/Answer.php
@@ -28,4 +28,8 @@ class Answer extends Model
             $answer->question->increment('answers_count');
         });
     }
+
+    public function getCreatedDateAttribute() {
+        return $this->created_at->diffForHumans();
+    }
 }
