cd64453d75ccd799a5d4874a168440dd6b6d4907
Antonio Fulgêncio <antoniofulg@gmail.com>
2020-01-24 18:41:32 -0300

    Create a basic Vue.js component

--- resources/views/answers/_answer.blade.php
+++ resources/views/answers/_answer.blade.php
@@ -21,10 +21,7 @@
             </div>
             <div class="col-4"></div>
             <div class="col-4">
-                @include ('shared._author', [
-                    'model' => $answer,
-                    'label' => 'answered'
-                ])
+                <user-info :model="{{ $answer }}" label="Answered"></user-info>
             </div>
         </div>                            
     </div>
