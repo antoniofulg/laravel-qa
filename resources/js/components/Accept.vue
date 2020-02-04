d9627dac28fb8909eb2f5df6cf68a9b71f95c62d
Antonio Fulgêncio <antoniofulg@gmail.com>
2020-02-04 14:31:26 -0300

    Rewrite the authorization policy into javascript code

--- resources/js/components/Accept.vue
+++ resources/js/components/Accept.vue
@@ -37,7 +37,7 @@ export default {
 
     computed: {
         canAccept () {
-            return true;
+            return this.authorize('accept', this.answer);
         },
 
         accepted () {
