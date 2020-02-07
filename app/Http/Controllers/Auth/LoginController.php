491be88426171ab6fd74a5a43556c2ba69a7fd59
Antonio Fulgêncio <antoniofulg@gmail.com>
2020-02-07 16:47:10 -0300

    Install & configure Laravel for API Authentication

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,5 +38,20 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function getToken(Request $request) {
+        $request->request->add([
+            'grant_type' => 'password',
+            'client_id' => '2',
+            'client_secret' => '24RGzrQtVItVLX9WS6aD2PGBnM5ZeZHJ3pG2J9ic',
+            'username' => $request->username,
+            'password' => $request->password
+        ]);
+
+        $requestToken = Request::create(env('APP_URL').'/oauth/token', 'post');
+        $response = Route::dispatch($requestToken);
+    
+        return $response;
     }
 }
